4f0edb6cace78fa109f4ae9c0251eeb088f413a8
sivavishnu26 <sivavishnukumaranj@gmail.com>
2026-01-30 11:55:52 +0530

    feat: initialize Firebase application with project configuration

---
+++ services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBR5wpTQHu2LPCCzMo6tRfqEnk3t4wPBOE",
+    authDomain: "lordgram-49f7e.firebaseapp.com",
+    projectId: "lordgram-49f7e",
+    storageBucket: "lordgram-49f7e.firebasestorage.app",
+    messagingSenderId: "496705427113",
+    appId: "1:496705427113:web:81dd0b5387905ae911bbb8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
